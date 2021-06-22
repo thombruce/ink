55486226d4545d17a2211d05da30a8bdf8b90cfc
Thom Bruce <thom@thombruce.com>
2021-06-22 01:54:55 +0100

    Add TNT and content

---
+++ pages/screenplays/index.vue
@@ -0,0 +1,18 @@
+<template lang='pug'>
+article
+  TntBlogList(:articles='articles')
+</template>
+
+<script>
+export default {
+  async asyncData ({ $content }) {
+    const articles = await $content('screenplays')
+      .where({ draft: { $ne: true } })
+      .sortBy('createdAt', 'desc')
+      .fetch()
+      .catch(() => {})
+
+    return { articles }
+  }
+}
+</script>
---
+++ tnt.config.js
@@ -0,0 +1,6 @@
+export default {
+  title: 'Screenplays',
+  description: 'My screenplays.',
+  url: 'https://screenplays.thombruce.com',
+  image: '/uploads/blog/rirri-p_swqcXUc1k-unsplash.jpg'
+}
