29e381e07514ee093a65d762b005569e5588b72c
Thom Bruce <thom@thombruce.com>
2021-08-07 22:01:42 +0100

    Add typewriter default image

--- tnt.config.js
+++ tnt.config.js
@@ -2,5 +2,5 @@ export default {
   title: 'Ink',
   description: 'My writing.',
   url: 'https://ink.thombruce.com',
-  image: '/uploads/blog/rirri-p_swqcXUc1k-unsplash.jpg'
+  image: '/uploads/blog/typewriter.jpg'
 }
